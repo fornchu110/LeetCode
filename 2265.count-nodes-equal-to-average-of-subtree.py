3dc2b322eae3dfc0787d7c50b25bb4207ae8c3fe
fornchu110 <81357897+fornchu110@users.noreply.github.com>
2023-11-02 13:57:08 +0800

    Add files via upload

--- 2265.count-nodes-equal-to-average-of-subtree.py
+++ 2265.count-nodes-equal-to-average-of-subtree.py
@@ -11,8 +11,13 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 # 計算node.val=自己+自己所有child的value平均數之node數量
 # 這種題目的基本一定是用recursive做dfs走訪所有node做計算
+
+# By DFS, Time: O(n), Space: O(logn)
+# space = O(logn)是因為Recursive中stack花費的空間, 但這題只是二元樹有可能最差O(n), 所以O(height), height = 樹高比較準確
+# 用DFS下去只是要同時記錄和和目前走過的數量, 一次回傳兩個參數
 class Solution:
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
         res = 0
@@ -29,7 +34,7 @@ class Solution:
             # 求出自己+自己所有child之value和
             value = lsub[0]+rsub[0]+root.val
             # 求出自己+自己所有child之node數量
-            count = lsub[1]+rsub[1] + 1
+            count = lsub[1]+rsub[1]+1
             # 看自己的value是否自己+自己所有child的value平均數
             # 若相等則+1
             if root.val == value // count:
@@ -37,6 +42,28 @@ class Solution:
             return value, count    
         dfs(root)
         return res
+    
+# By DFS, Time: O(n), Space: O(logn)
+# 改成將def和res寫在class下
+# class Solution:
+#     def __init__(self):
+#         self.res = 0
+    
+#     def dfs(self, root):
+#         if not root:
+#             return 0, 0
+#         lsub = self.dfs(root.left)
+#         rsub = self.dfs(root.right)
+#         value = lsub[0]+rsub[0]+root.val
+#         count = lsub[1]+rsub[1]+1
+#         if root.val==value//count:
+#             self.res += 1
+#         return value, count
+
+#     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
+#         self.dfs(root)
+#         return self.res
+
              
 # @lc code=end
 
