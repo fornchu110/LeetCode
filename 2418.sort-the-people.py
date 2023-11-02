77c5deca687cb8ca3d4b4d23e88df06c2be97d82
fornchu110 <81357897+fornchu110@users.noreply.github.com>
2023-11-02 15:05:46 +0800

    Add files via upload

--- 2418.sort-the-people.py
+++ 2418.sort-the-people.py
@@ -5,20 +5,28 @@
 #
 
 # @lc code=start
-# By zip, time: O(n), space: O(1)
+# By zip and 列表生成式, time: O(n), space: O(1)
 # 要求return照身高由高到低排序後的對應人名
 # 用zip()將身高和名字一一走訪, 只加入人名
 # zip後的list也可以排序
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        res = []
-        tmp = list(zip(heights, names))
-        # 將zip後的list排序, 因為sorted()是從小到大排序, reverse = True參數變從大到小排序
-        tmp  = sorted(tmp, reverse = True)
-        # 找排序過後的name
-        for height, name in tmp:
-            res.append(name)
+        tmp = sorted(zip(heights, names), reverse = True)
+        # res = [*zip(*tmp)][1]
+        res = [name for height, name in tmp]
         return res
+        
+# By zip , time: O(n), space: O(1)
+# class Solution:
+#     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+#         res = []
+#         tmp = list(zip(heights, names))
+#         # 將zip後的list排序, 因為sorted()是從小到大排序, reverse = True參數變從大到小排序
+#         tmp  = sorted(tmp, reverse = True)
+#         # 找排序過後的name
+#         for height, name in tmp:
+#             res.append(name)
+#         return res
 
 # @lc code=end
 
