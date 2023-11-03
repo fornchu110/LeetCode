ff91dbaa7353684da4be65c9878ceab5d76ca364
fornchu110 <fornchu110@gmail.com>
2023-11-04 02:14:35 +0800

    解決合併衝突

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #
 # @lc app=leetcode id=2 lang=python3
 #
@@ -12,16 +11,16 @@
 #         self.val = val
 #         self.next = next
 
-# By simulation, time: O(max(m, n)), space: O(1), m¡Bn¬O¨âlinklistªºªø«×
-# ¦]inputªº¨âlinklist¤w¸g¬O°f§Ç(reverse order), ©Ò¥H¤£¥Î¯S§Oreverse
+# By simulation, time: O(max(m, n)), space: O(1), mã€næ˜¯å…©linklistçš„é•·åº¦
+# å› inputçš„å…©linklistå·²ç¶“æ˜¯é€†åº(reverse order), æ‰€ä»¥ä¸ç”¨ç‰¹åˆ¥reverse
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         carry = 0
-        # cur©Mdummy±q¦P¼Ë¤@­Ónode¶}©l
+        # curå’Œdummyå¾åŒæ¨£ä¸€å€‹nodeé–‹å§‹
         cur = dummy = ListNode()
-        # ¦pªGcur§ïÅÜ¥Ø«e³o­Ónodeªº¤º®e, dummy«ü¦Vªº¥Ø«e³o­Ónode¤º®e¤]·|§ïÅÜ
-        # ±q¦ÓÅıdummy.next²£¥Í
-        # ¦ı·ícur«ü¦V§Oªºnode«á, dummy«ü¦Vªºnode³£¨SÅÜ
+        # å¦‚æœcuræ”¹è®Šç›®å‰é€™å€‹nodeçš„å…§å®¹, dummyæŒ‡å‘çš„ç›®å‰é€™å€‹nodeå…§å®¹ä¹Ÿæœƒæ”¹è®Š
+        # å¾è€Œè®“dummy.nextç”¢ç”Ÿ
+        # ä½†ç•¶curæŒ‡å‘åˆ¥çš„nodeå¾Œ, dummyæŒ‡å‘çš„nodeéƒ½æ²’è®Š
         while l1 is not None or l2 is not None or carry!=0:
             if l1 is not None:
                 l1val = l1.val
@@ -42,52 +41,6 @@ class Solution:
                 l2 = l2.next
         return dummy.next
 
-# @lc code=end
-
-=======
-#
-# @lc app=leetcode id=2 lang=python3
-#
-# [2] Add Two Numbers
-#
-
-# @lc code=start
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-# By simulation, time: O(max(m, n)), space: O(1), m¡Bn¬O¨âlinklistªºªø«×
-# ¦]inputªº¨âlinklist¤w¸g¬O°f§Ç(reverse order), ©Ò¥H¤£¥Î¯S§Oreverse
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry = 0
-        # cur©Mdummy±q¦P¼Ë¤@­Ónode¶}©l
-        cur = dummy = ListNode()
-        # ¦pªGcur§ïÅÜ¥Ø«e³o­Ónodeªº¤º®e, dummy«ü¦Vªº¥Ø«e³o­Ónode¤º®e¤]·|§ïÅÜ
-        # ±q¦ÓÅıdummy.next²£¥Í
-        # ¦ı·ícur«ü¦V§Oªºnode«á, dummy«ü¦Vªºnode³£¨SÅÜ
-        while l1 is not None or l2 is not None or carry!=0:
-            if l1 is not None:
-                l1val = l1.val
-            else:
-                l1val = 0
-            if l2 is not None:
-                l2val = l2.val
-            else:
-                l2val =  0
-            tmp = l1val+l2val+carry
-            carry = tmp//10
-            tmp %= 10
-            cur.next = ListNode(tmp) 
-            cur = cur.next 
-            if l1 is not None:
-                l1 = l1.next
-            if l2 is not None:
-                l2 = l2.next
-        return dummy.next
 
 # @lc code=end
 
->>>>>>> 6861f1229a47360993e49170b9b1be7c1dd4f215
