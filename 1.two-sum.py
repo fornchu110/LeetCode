54b78a3cb714b9bf7155c602b185f63adf067442
fornchu110 <fornchu110@gmail.com>
2023-11-04 10:20:10 +0800

    Recode to UTF-8

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -1,22 +1,27 @@
 #
+
 # @lc app=leetcode id=1 lang=python3
+
 #
 # [1] Two Sum
 #
 
 # @lc code=start
+# 給一陣列nums和整數target, 找出nums內兩兩和等同target之元素index
 
-#By hashtable
+#By hashtable, time: O(n), space: O(n)
+# 一邊走訪n一邊查找和建立hash, 當遇到target-自己的元素存在於hash代表找到res
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashtable = dict()
         for i in range(len(nums)):
             if target-nums[i] in hashtable:
-                return [hashtable[target - nums[i]], i]
+                return [hashtable[target-nums[i]], i]
             hashtable[nums[i]] = i
         return []
 
-#By for loop
+#By for loop, time: O(n^2), space: O(1)
+# 基本的做法, 雙迴圈兩兩比較
 # class Solution:
 #     def twoSum(self, nums: List[int], target: int) -> List[int]:
 #         n = len(nums)
--- 67.add-binary.py
+++ 67.add-binary.py
@@ -9,14 +9,14 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         res, carry = '', 0
         i, j = len(a)-1, len(b)-1
-        while (i >=0 or j >=0 or carry):
+        while (i>=0 or j>=0 or carry):
             if i>=0:
                 carry += int(a[i])
                 i -= 1
             if j>=0:
                 carry += int(b[j])
                 j -= 1
-            res = str(carry % 2) + res
+            res = str(carry&1)+res
             carry //= 2
         return res
 
