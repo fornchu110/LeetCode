cf196298b066c2fea8c233be86d5bf35ce63f214
fornchu110 <fornchu110@gmail.com>
2023-11-04 10:23:37 +0800

    Recode to UTF-8

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -3,20 +3,20 @@
 #
 # [70] Climbing Stairs
 #
-#By DP
+#By DP, time: O(n), space: O(1)
 #基本想法n階只有兩種可能:差一階時跨1步和差兩階時跨2步
 #所以等同f(n) = f(n-1)+f(n-2)
 #假設n = 5, 答案等同到3階的方法數+到4階的方法數
 class Solution:
     def climbStairs(self, n: int) -> int:
-        p = 0
-        q = 0
-        r = 1
+        f_prevprev = 0
+        f_perv = 0
+        f_n = 1
         for i in range(n):
-            p = q
-            q = r
-            r = p+q
-        return r
+            f_prevprev = f_perv
+            f_perv = f_n
+            f_n = f_prevprev+f_perv
+        return f_n
 
 # @lc code=end
 
